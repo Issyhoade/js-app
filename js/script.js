95a84d5506e83a1f281a07841c6f1d4b74546ca0
Issy Hoade <ilhoade@gmail.com>
2025-06-20 18:22:01 +0100

    Made new variable

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// this array contains objects with properties name, height, and types
+// each object represents a Pokémon with its respective attributes 
+
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 7,
+    types: ["grass", "poison"]
+  },
+  {
+    name: "Charmander",
+    height: 6,
+    types: ["fire"]
+  },
+  {
+    name: "Squirtle",
+    height: 5,
+    types: ["water"]
+  },
+  {
+    name: "Beedrill",
+    height: 1,
+    types: ["bug", "poison"]
+  }
+];
+// Added the same keys to avoid errors //
