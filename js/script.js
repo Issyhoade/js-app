9076f19d0692ce3b461abe5953ed0c5f84f112e3
Issy Hoade <ilhoade@gmail.com>
2025-08-13 09:43:52 +0100

    Updated to merge

--- js/script.js
+++ js/script.js
@@ -3,106 +3,32 @@
 
 /* Pokémon List Example */
 
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 7,
-    types: ["grass", "poison"]
-  },
-  {
-    name: "Charmander",
-    height: 6,
-    types: ["fire"]
-  },
-  {
-    name: "Squirtle",
-    height: 5,
-    types: ["water"]
-  },
-  {
-    name: "Beedrill",
-    height: 1,
-    types: ["bug", "poison"]
-  }
-];
+let pokemonRepository = (function() {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 7, types: ["grass", "poison"] },
+    { name: "Charmander", height: 6, types: ["fire"] },
+    { name: "Squirtle", height: 5, types: ["water"] },
+    { name: "Beedrill", height: 1, types: ["bug", "poison"] }
+  ];
 
-
-pokemonList.forEach(function(pokemon) {
-
-  console.log (pokemon.name);
-});
-
-// Check if the height is greater than 6 (choose a value where only ONE Pokémon qualifies)
-  if (pokemon.height > 6) {
-    output += " - Wow, that’s big!"; // Add special note
+  function getAll() {
+    return pokemonList;
   }
 
-  // Write the output to the webpage
-  document.write(output + "<br>");
-
-  let pokemonRepository = (function() {
-  })();
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-  console.log(pokemon);
-});
-
-(function() {
-  // This is an immediately invoked function expression (IIFE)
-
-let pokemonRepository = ( function () {
-}) ();
-
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 7,
-    types: ["grass", "poison"]
-  },
-  {
-    name: "Charmander",
-    height: 6,
-    types: ["fire"]
-  },
-  {
-    name: "Squirtle",
-    height: 5,
-    types: ["water"]
-  },
-  {
-    name: "Beedrill",
-    height: 1,
-    types: ["bug", "poison"]
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-];
 
-function getAllPokemon() {
-  return pokemonList; // Return the entire Pokémon list
-} 
-
-function addPokemon(pokemon) {
-   pokemonList.push(pokemon); // Add a new Pokémon to the list
-}
-
-return {
-  getAll: getAllPokemon, // Expose the getAllPokemon function
-  add: addPokemon // Expose the addPokemon function
-};
-
+  return {
+    getAll: getAll,
+    add: add
+  };
 })();
 
-
-
-
-// Corrected divide function
-function divide(dividend, divisor) {
-  // Check if the divisor is zero to avoid division by zero error
-  if (divisor === 0) {
-    return "You're trying to divide by zero!"
+pokemonRepository.getAll().forEach(function(pokemon) {
+  let output = pokemon.name + " (height: " + pokemon.height + ")";
+  if (pokemon.height > 6) {
+    output += " - Wow, that’s big!";
   }
-  let result = dividend / divisor // Perform the division operation
-  return result
-}
-
-console.log("Result of 4 / 2:", divide(4, 2));
-console.log("Result of 10 / 0:", divide(10, 0));
+  document.write(output + "<br>");
+});
